44826f8a69fdfba90a002e2edbd1580c1aa842a5
Vladislav <98273582+pastilkaxo@users.noreply.github.com>
2023-04-27 22:36:11 +0300

    Add files via upload

--- src/storage.js
+++ src/storage.js
@@ -6,11 +6,26 @@ fetch('/src/data.xml')
     const parser = new DOMParser();
     const xmlDoc = parser.parseFromString(xml, "text/xml");
 
+   // part 1
+   const adHead = xmlDoc.getElementsByTagName("adHead")[0].childNodes[0].nodeValue;
+   const phHead   = xmlDoc.getElementsByTagName("phHead")[0].childNodes[0].nodeValue;
+   const wHead = xmlDoc.getElementsByTagName("wHead")[0].childNodes[0].nodeValue;
+
+   const sumAd = document.getElementById("adHead");
+   sumAd.innerHTML = `${adHead}`
+
+  const sumPh = document.getElementById("phHead");
+  sumPh.innerHTML = `${phHead}`
+
+  const sumW = document.getElementById("wHead");
+  sumW.innerHTML = `${wHead}`;
+
+    // part 2
     const address = xmlDoc.getElementsByTagName("address")[0].childNodes[0].nodeValue;
     const phone = xmlDoc.getElementsByTagName("phone")[0].childNodes[0].nodeValue;
     const time = xmlDoc.getElementsByTagName("time")[0].childNodes[0].nodeValue;
     const wkd = xmlDoc.getElementsByTagName("weekend")[0].childNodes[0].nodeValue;
-
+        
     // Вставляем данные в footer
     const addressEl = document.getElementById('address');
     addressEl.innerHTML = `${address}`;
@@ -24,4 +39,8 @@ fetch('/src/data.xml')
     const wkdEl = document.getElementById('wkd');
     wkdEl.innerHTML = `${wkd}`;
 
+
+
+
+ 
   });
